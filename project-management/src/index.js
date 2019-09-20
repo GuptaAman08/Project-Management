36f596dd359ed201a812ba7ee19e62f9eaa0d5cf
GuptaAman08 <2015aman.a@ves.ac.in>
2019-09-20 09:24:35 +0530

    using config object as second parameter to ensure loading of DOM after auth has been done and using profile object on firebase object which is property of state

--- project-management/src/index.js
+++ project-management/src/index.js
@@ -15,13 +15,20 @@ const store = createStore(rootReducer,
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
         reduxFirestore(fbconfig),
-        reactReduxFirebase(fbconfig)
+        reactReduxFirebase(fbconfig, {useFirestoreForProfile: true, userProfile: 'users' ,attachAuthIsReady: true})
     )    
 )
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// By using this method we are able to load DOM only when firebase auth is ready 
+store.firebaseAuthIsReady.then( () => {
+    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+})
+    
+
+
+
