01736323e9b662d35650146b2590ba17356bc0f5
GuptaAman08 <2015aman.a@ves.ac.in>
2019-09-20 09:21:20 +0530

    listening to different action type in authRed and using inbuilt firebase and firesstore reducer to sync redux state with corressponding collection in firestore

--- project-management/src/store/reducers/authReducer.js
+++ project-management/src/store/reducers/authReducer.js
@@ -1,7 +1,44 @@
-const initState = {}
+const initState = {
+    login_status: null
+}
 
 const authReducer = (state = initState, action) => {
-    return state
+    switch (action.type){
+        case 'LOGIN_ERROR':
+            console.log('Login Failed');
+            return {
+                ...state,
+                login_status: 'Login Failed'
+            }
+
+        case 'LOGIN_SUCCESS':
+            console.log('Login Success');
+            return {
+                ...state,
+                login_status: null
+            }
+
+        case 'LOGOUT_SUCCESS':
+            console.log('signout Success');
+            return state
+
+        case 'SIGNUP_SUCCESS':
+            console.log('signup success');
+            return{
+                ...state,
+                login_status: null
+            }
+            
+        case 'SIGNUP_ERROR':
+            console.log('signup error');
+            return{
+                ...state,
+                login_status: action.err.message
+            }   
+        
+        default:
+            return state
+    }
 }
 
 export default authReducer;
